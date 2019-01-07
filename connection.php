3a5eb27633838b13fd92182e4b76e6ad6b4baafc
Tomi Putro Utomo <tomi.putro.utomo@gmail.com>
2019-01-07 21:19:11 +0700

    change connection string

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$host="localhost";
-$username="root"; 
-$password="123456"; 
-$dbname="fabelio"; 
+$host="ipobfcpvprjpmdo9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username="z5mg9yrkdvgian3z"; 
+$password="n0w0qjx8j9lhrpwq"; 
+$dbname="q0byv37q7jy899k9"; 
 
 $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
